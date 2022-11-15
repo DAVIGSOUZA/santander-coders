7705a5ed2b5c49d6f15445bb73372f8df45abe37
Davi Garcia <davigs@hotmail.com>
2022-11-14 21:18:15 -0300

    add exercicios aula03 questao03

--- src/Exercicios/Aula03/Questao03/Questão03.java
+++ src/Exercicios/Aula03/Questao03/Questão03.java
@@ -1,4 +1,5 @@
 package Exercicios.Aula03.Questao03;
+import java.util.Scanner;
 
 /*.
     ENUNCIADO
@@ -14,5 +15,41 @@ package Exercicios.Aula03.Questao03;
         O programa devera imprimir na tela o nome e preço do refrigerante selecionado, utilize switch.
  */
 
+
 public class Questão03 {
+    public static void main(String[] args) {
+        String[] productNames = {"Coca-Cola", "Coca-Cola 0", "Pepsi", "Guaraná Antarctica", "Fanta Laranja", "Água"};
+        float[] productPrices = {5, 4.5f, 4.4f, 3.5f, 4.23f, 2.5f};
+
+        System.out.println("Informe o número do produto desejado.");
+
+        for (int i = 0; i < productNames.length; i++) {
+            System.out.printf("%s - %s - R$ %s \n", (i + 1), productNames[i], productPrices[i] );
+        }
+
+        int choice = getInputWithScanner();
+
+//        System.out.printf("Você escolheu %s - R$ %s", productNames[choice - 1], productPrices[choice - 1]);
+
+        switch (choice) {
+            case 1 -> System.out.printf("%s - R$ %s", productNames[0], productPrices[0]);
+            case 2 -> System.out.printf("%s - R$ %s", productNames[1], productPrices[1]);
+            case 3 -> System.out.printf("%s - R$ %s", productNames[2], productPrices[2]);
+            case 4 -> System.out.printf("%s - R$ %s", productNames[3], productPrices[3]);
+            case 5 -> System.out.printf("%s - R$ %s", productNames[4], productPrices[4]);
+            case 6 -> System.out.printf("%s - R$ %s", productNames[5], productPrices[5]);
+            default -> throw new IllegalStateException("Escolha inválida: " + choice);
+        }
+
+    }
+
+    static int getInputWithScanner() {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        sc.close();
+
+        return input;
+    }
 }
